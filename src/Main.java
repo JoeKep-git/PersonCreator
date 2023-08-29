06c933505221bc9021d1357f7aad23abde072014
JoeK <ohhjassassin@gmail.com>
2023-08-29 15:04:52 +0100

    added 2 people and printing their values

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a new Person object
+        //height cm, weight lbs
+        Person person = new Person("James Peterson", (byte) 34, 180, 73.5f);
+        Person person1 = new Person("James", (byte) 25, 171, 68.2f);
+
+        System.out.println("Person 1: " +
+                person.getName() + ", " +
+                person.getAge() + ", " +
+                person.getHeight() + ", " +
+                person.getWeight());
+
+        System.out.println("Person 2: " +
+                person1.getName() + ", " +
+                person1.getAge() + ", " +
+                person1.getHeight() + ", " +
+                person1.getWeight());
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    private String name;
+    private byte age;
+    private int height;
+    private float weight;
+
+    //height cm, weight lbs
+    public Person(String name, byte age, int height, float weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
